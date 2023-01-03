fdf6fe4ae6db40a4888fbc72d9c0fcd52668c810
TaliahOsama <taliahrehman957@gmail.com>
2023-01-03 20:15:10 +0100

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void prompt() {
+        System.out.println("Enter a number");
+    }
+
+    public static int parse(String a) {
+        int b = Integer.parseInt(a);
+        return b;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Choose one of the following operations: ");
+
+        System.out.println("Addition, Subtraction, Multiplication, Division?");
+        String type = scan.nextLine();
+
+        if (type.equals("Addition")) {
+            prompt();
+            String num1 = scan.nextLine();
+            prompt();
+            String num2 = scan.nextLine();
+            System.out.println("Result is:");
+            System.out.println(parse(num1) + parse(num2));
+        } else if (type.equals("Subtraction")) {
+            prompt();
+            String num1 = scan.nextLine();
+            prompt();
+            String num2 = scan.nextLine();
+            System.out.println("Result is: ");
+            System.out.println(parse(num1) - parse(num2));
+
+        }
+        else if (type.equals("Multiplication")) {
+            prompt();
+            String num1 = scan.nextLine();
+            prompt();
+            String num2 = scan.nextLine();
+            System.out.println("Result is: ");
+            System.out.println(parse(num1) * parse(num2));
+        }
+        else if (type.equals("Division")) {
+            prompt();
+            String num1 = scan.nextLine();
+            prompt();
+            String num2 = scan.nextLine();
+            System.out.println("Result is: ");
+            System.out.println(parse(num1) / parse(num2));
+        }
+    }
+}
